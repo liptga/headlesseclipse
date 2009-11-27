628f775b700a0e0d147319bb9f56e9255208989c
gabor.liptak <gabor.liptak@8f045908-b728-11de-a71d-8f2b2e058ad8>
2009-11-27 06:59:58 +0000

    Issue 7: eclipse frozes sometime. As a workaround we call system exit after work.

--- com.ind.eclipse.headlessworkspace/src/com/ind/eclipse/headlessworkspace/HeadLessAdvisor.java
+++ com.ind.eclipse.headlessworkspace/src/com/ind/eclipse/headlessworkspace/HeadLessAdvisor.java
@@ -55,6 +55,7 @@ public class HeadLessAdvisor extends WorkbenchAdvisor
 		SysOutProgressMonitor.out.println("POSTSTARTUP");
 		PlatformUI.getWorkbench().close();
 		SysOutProgressMonitor.out.println("WORKBENCH CLOSED");
+		System.exit(0);
 	}
 
 	@Override
